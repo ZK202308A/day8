9d406bb086e32730b6885d7e80bd2f0a64851581
zk202308A <zk202308A@gmail.com>
2024-07-16 10:50:15 +0900

    2nd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,23 @@
 package org.example;
 
+import org.example.pizza.CommonUI;
+import org.example.pizza.CreateUI;
+import org.example.pizza.ReadUI;
+import org.example.pizza.TotalUI;
+
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        CommonUI[] arr = {new CreateUI(in), new ReadUI(in)};
+
+        TotalUI totalUI = new TotalUI(in, arr);
+
+        totalUI.execute();
 
 
 
